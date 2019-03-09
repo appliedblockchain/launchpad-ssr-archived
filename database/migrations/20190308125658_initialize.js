46cae86914c1e2560649059f94888209a0a25c2d
makevoid <makevoid@gmail.com>
2019-03-09 22:50:38 +0000

    remove unwanted schema - main branch still has the full schema for the new project

--- database/migrations/20190308125658_initialize.js
+++ database/migrations/20190308125658_initialize.js
@@ -30,56 +30,6 @@ exports.up = async knex => {
     t.timestamps(false, true)
   })
 
-  return
-  // we don't need this in the sample app
-
-  await knex.schema.createTable("events", t => {
-    t.increments("id").primary()
-    t.string("title")
-    t.text("descrition")
-    t.string("location")
-    t.timestamp("start")
-    t.timestamp("ends")
-    t.string("notes")
-    t.string("notes")
-    t.json("guests")
-      .references("id")
-      .inTable("users")
-    t.integer("created_by")
-      .references("id")
-      .inTable("users")
-    t.boolean("private")
-    t.timestamps(false, true)
-  })
-
-  await knex.schema.createTable("documents", t => {
-    t.increments("id").primary()
-    t.string("filename")
-    t.string("type")
-    t.integer("receiver")
-      .references("id")
-      .inTable("users")
-    t.string("origin")
-    t.string("eta")
-    t.text("description")
-    t.integer("created_by")
-      .references("id")
-      .inTable("users")
-    t.enu("status", ["incomming", "sent"]).defaultTo("incomming")
-    t.timestamps(false, true)
-  })
-
-  await knex.createTable("vessels", t => {
-    t.increments("id").primary()
-    t.string("name")
-    t.timestamp("eta")
-    t.string("block")
-    t.string("comm")
-    t.string("so")
-    t.text("description")
-    t.enu('status',['incoming, delayed']).defaultTo('incomming')
-    t.timestamp(false, true)
-  })
 }
 
 exports.down = async knex => {
