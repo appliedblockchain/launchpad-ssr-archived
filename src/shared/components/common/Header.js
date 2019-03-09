019cc2487ea1a32142ff4fb2d5037750609d3c31
Tiago <t.dfgomes@icloud.com>
2019-03-09 23:41:39 +0000

    feat:add ab-eslint

--- src/shared/components/common/Header.js
+++ src/shared/components/common/Header.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react'
-import Nav from '../nav/Nav'
+import React, { Component } from "react";
+import Nav from "../nav/Nav";
 
 class Header extends Component {
   static async getInitialProps({ req, res, match, history, location, ...ctx }) {
-    const { users, companies } = ctx.data
-    return { users, companies }
+    const { users, companies } = ctx.data;
+    return { users, companies };
   }
   render() {
     return (
       <div>
-        <a href="/">
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>My Title</title>
           <link rel="stylesheet" href="/style.css" />
+        </Helmet>
+        <a href="/">
           <div className="header">
             <img src="/img/react.svg" className="logo" alt="logo" />
             <h2>Welcome to AB</h2>
@@ -18,11 +22,11 @@ class Header extends Component {
         </a>
         <Nav />
       </div>
-    )
+    );
   }
 }
 
-export default Header
+export default Header;
 
 // TODO: setup and use 'react-helmet'
 // react-helmet will take care of injecting the <link/> tag and customize the app title to something other than "Welcome to the Afterparty" ^^ - TT
