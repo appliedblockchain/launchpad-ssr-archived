ec3e34e6df0fe263aa8ddf6f3c29144d604fadd1
Tiago <t.dfgomes@icloud.com>
2019-03-11 23:27:31 +0000

    refactor: move to statless  components

--- src/shared/components/home/Home.js
+++ src/shared/components/home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import Header from '../common/Header'
 
 class Home extends Component {
@@ -7,18 +8,25 @@ class Home extends Component {
     return { currentUser }
   }
 
+  static propTypes = {
+    currentUser: PropTypes.object
+  }
+
   currentUserText(user) {
-    return (user && (<p>You&#39;re logged in as {user.email}</p>))
+    return user && <p>You&#39;re logged in as {user.email}</p>
   }
 
   render() {
     const { currentUser } = this.props
     return (
       <div className="Home Page">
-        <Header />
+        <Header currentUser={currentUser} />
         <section className="content">
           <p className="intro">
-            To get started, view <a href="https://github.com/appliedblockchain/base-ssr-app/tree/master/src/shared/components">this directory, where all the components reside</a>
+            To get started, view{' '}
+            <a href="https://github.com/appliedblockchain/base-ssr-app/tree/master/src/shared/components">
+              this directory, where all the components reside
+            </a>
           </p>
 
           {this.currentUserText(currentUser)}
--- src/shared/components/login/Login.js
+++ src/shared/components/login/Login.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Header from '../common/Header'
 
-class Login extends Component {
-  render() {
-    return (
-      <div className="Login Page">
-        <Header />
-        <section className="content">
-          <form method="post" action="/sessions/create">
-            <label htmlFor="resource-name"><strong>Email:</strong></label>
-            <div className="s7" />
-            <input id="resource-name" type="email" name="email" />
-            <div className="s20" />
-            <label htmlFor="resource-name"><strong>Password:</strong></label>
-            <div className="s7" />
-            <input id="resource-name" type="password" name="password" />
-            <div className="s20" />
-            <input type="submit" value="Log in" />
-          </form>
-        </section>
-      </div>
-    )
-  }
-}
+const Login = () => (
+  <div className="Login Page">
+    <Header />
+    <section className="content">
+      <form method="post" action="/sessions/create">
+        <label htmlFor="resource-name"><strong>Email:</strong></label>
+        <div className="s7" />
+        <input id="resource-name" type="email" name="email" />
+        <div className="s20" />
+        <label htmlFor="resource-name"><strong>Password:</strong></label>
+        <div className="s7" />
+        <input id="resource-name" type="password" name="password" />
+        <div className="s20" />
+        <input type="submit" value="Log in" />
+      </form>
+    </section>
+  </div>
+)
 
 export default Login
