c58ba8cf492f7f559a8bea3423178aef1320867f
makevoid <makevoid@gmail.com>
2019-03-11 20:59:52 +0000

    add lib for getting the user out of the session

---
+++ src/server/utils/currentSession.js
@@ -0,0 +1,18 @@
+import findOne from '../utils/db/findOne'
+
+const currentSession = async (ctx) => {
+  const { session } = ctx
+  const { userId } = session
+  console.log('Auth - userId:', userId)
+  if (!userId) {
+    return { user: null }
+  }
+  let user = await findOne('users', userId)
+  if (user) {
+    user = user[0]
+    console.log(`Auth - user: ${user.email}`)
+    return { user }
+  }
+}
+
+export default currentSession
