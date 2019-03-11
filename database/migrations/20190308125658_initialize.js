0558cd1f6a811e0a941040c2dbb4ea559ea28bfc
Dean Richer <dpbricher@gmail.com>
2019-03-11 11:10:50 +0000

    fix: fixed database initialize migration

--- database/migrations/20190308125658_initialize.js
+++ database/migrations/20190308125658_initialize.js
@@ -1,6 +1,6 @@
 exports.up = async knex => {
   await knex.schema.createTable("users", t => {
-    t.increments("id").primary();
+    t.increments("id")
     t.string("name");
     t.string("email")
       .unique()
@@ -8,77 +8,100 @@ exports.up = async knex => {
     t.string("phone");
     t.string("password").notNullable();
     t.string("location");
-    t.integer("role")
-      .references("id")
-      .inTable("user_role");
-    t.integer("company")
-      .references("id")
-      .inTable("company_name");
     t.timestamps(false, true);
   });
 
   await knex.schema.createTable("user_role", t => {
-    t.increments("id").primary();
+    t.increments("id")
     t.string("name").notNullable();
     t.timestamps(false, true);
   });
 
   await knex.schema.createTable("company_name", t => {
-    t.increments("id").primary();
+    t.increments("id")
     t.string("name").notNullable();
     t.timestamps(false, true);
   });
 
   await knex.schema.createTable("events", t => {
-    t.increments("id").primary();
+    t.increments("id")
     t.string("title");
-    t.text("descrition");
+    t.text("description");
     t.string("location");
     t.timestamp("start");
     t.timestamp("ends");
     t.string("notes");
-    t.string("notes");
-    t.json("guests")
-      .references("id")
-      .inTable("users");
-    t.integer("created_by")
-      .references("id")
-      .inTable("users");
     t.boolean("private");
     t.timestamps(false, true);
   });
 
   await knex.schema.createTable("documents", t => {
-    t.increments("id").primary();
+    t.increments("id")
     t.string("filename");
     t.string("type");
-    t.integer("receiver")
-      .references("id")
-      .inTable("users");
     t.string("origin");
-    t.string("eta");
+    t.timestamp("eta");
     t.text("description");
-    t.integer("created_by")
-      .references("id")
-      .inTable("users");
-    t.enu("status", ["incomming", "sent"]).defaultTo("incomming");
+    t.enu("status", ["incoming", "sent"]).defaultTo("incoming");
     t.timestamps(false, true);
   });
 
-  await knex.createTable("vessels", t => {
-    t.increments("id").primary();
+  await knex.schema.createTable("vessels", t => {
+    t.increments("id")
     t.string("name");
     t.timestamp("eta");
     t.string("block");
     t.string("comm");
     t.string("so");
     t.text("description");
-    t.enu('status',['incoming, delayed']).defaultTo('incomming')
-    t.timestamp(false, true);
+    t.enu("status", ["incoming", "delayed"]).defaultTo("incoming")
+    t.timestamps(false, true);
+  });
+
+  await knex.schema.table("users", t => {
+    t.integer("role")
+      .references("id")
+      .inTable("user_role");
+    t.integer("company")
+      .references("id")
+      .inTable("company_name");
+  });
+
+  await knex.schema.table("events", t => {
+    t.integer("guest")
+      .references("id")
+      .inTable("users");
+    t.integer("created_by")
+      .references("id")
+      .inTable("users");
+  });
+
+  await knex.schema.table("documents", t => {
+    t.integer("receiver")
+      .references("id")
+      .inTable("users");
+    t.integer("created_by")
+      .references("id")
+      .inTable("users");
   });
 };
 
 exports.down = async knex => {
+  await knex.schema.table("users", t => {
+    t.dropForeign("role");
+    t.dropForeign("company");
+  })
+
+  await knex.schema.table("events", t => {
+    t.dropForeign("guest");
+    t.dropForeign("created_by");
+  })
+
+  await knex.schema.table("documents", t => {
+    t.dropForeign("receiver");
+    t.dropForeign("created_by");
+  })
+
   await knex.schema.dropTableIfExists("users");
   await knex.schema.dropTableIfExists("user_role");
   await knex.schema.dropTableIfExists("company_name");
